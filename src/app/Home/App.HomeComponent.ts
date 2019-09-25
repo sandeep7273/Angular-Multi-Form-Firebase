d970a7cf04757da7b043858fac085f7d5da5e1d1
sandeep7273 <54482803+sandeep7273@users.noreply.github.com>
2019-09-25 18:52:14 +0530

    MultiStepForm With firebase database

---
+++ src/app/Home/App.HomeComponent.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core'
+import { User } from './App.Home.model'
+import {HttpClient} from '@angular/common/http'
+import { FormService} from '../form.service'
+import {NgForm, FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+
+@Component({
+    templateUrl: './App.HomeView.html',
+  styleUrls: ['../App.CommonStyle.css']
+
+})
+
+export class HomeComponent implements OnInit {
+
+  UserModel : User = new User();
+  UserModels : Array<User> = new Array<User>();
+  disable : boolean = false;
+  formSignUpGroup: FormGroup= null;
+  
+  constructor(public formService : FormService, public httpc : HttpClient ){
+      var _builder = new FormBuilder;
+      this.formSignUpGroup = _builder.group({});
+      this.formSignUpGroup.addControl('UserNameControl', new FormControl('', Validators.required));
+
+      var validationCollection = [];
+      validationCollection.push(Validators.required);
+      validationCollection.push(Validators.pattern("^[0-9]{10,10}$"));
+      //  var validationCollectionPassword = [];
+      //  validationCollectionPassword.push(Validators.required);
+      //  validationCollectionPassword.push(Validators.pattern("^[a-z]{8,8}$"));
+      var validationEmail =[];
+      validationEmail.push(Validators.required);
+      validationEmail.push(Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"))
+
+      // this.formSignUpGroup.addControl('UserEmailControl',
+      //                          new FormControl('', Validators.required));
+      // this.formSignUpGroup.addControl('UserPasswordControl',
+      //                          new FormControl('', Validators.compose(validationCollectionPassword)));
+      // this.formSignUpGroup.addControl('UserRePasswordControl',
+      //                          new FormControl('', Validators.compose(validationCollectionPassword)));
+      this.formSignUpGroup.addControl('UserContactControl', 
+                               new FormControl('', Validators.compose(validationCollection)));
+      
+      this.formSignUpGroup.addControl('UserEmailControl', 
+                               new FormControl('', Validators.compose(validationEmail)));
+  }
+
+  addUser(User){
+    this.formService.items = [];
+    this.formService.addUser(this.UserModel);
+    this.formService.createCustomer(this.UserModel);
+  }
+
+  hasError(controlString: string, typeOfValidatior: string): boolean{
+    this.disable = true;
+    return this.formSignUpGroup.controls[controlString].hasError(typeOfValidatior);
+  }
+
+  ngOnInit() {
+  }
+  
+}
---
+++ src/app/Registration/App.RegistrationComponent.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core'
+import {NgForm, FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+
+import { from } from 'rxjs';
+import {HttpClient} from '@angular/common/http'
+import { FormService } from '../form.service'
+import { User } from '../Home/App.Home.model';
+@Component({
+    templateUrl: './App.RegistrationView.html',
+  styleUrls: ['../App.CommonStyle.css']
+
+})
+
+export class RegistrationComponent implements OnInit {
+
+  UserModel : User = new User();
+  UserModels : Array<User> = new Array<User>();
+  items;
+  formSignUpGroup: FormGroup= null;
+
+constructor( public formService: FormService,  public httpc : HttpClient ) {
+
+  var _builder = new FormBuilder;
+  this.formSignUpGroup = _builder.group({});
+  this.formSignUpGroup.addControl('UserHouseControl', new FormControl('', Validators.required));
+  this.formSignUpGroup.addControl('UserCityControl', new FormControl('', Validators.required));
+  this.formSignUpGroup.addControl('UserDisttControl', new FormControl('', Validators.required));
+  this.formSignUpGroup.addControl('UserStateControl', new FormControl('', Validators.required));
+  var validationCollection = [];
+  validationCollection.push(Validators.required);
+  validationCollection.push(Validators.pattern("^[0-9]{6,6}$"));
+   var validationCollectionPassword = [];
+   validationCollectionPassword.push(Validators.required);
+   validationCollectionPassword.push(Validators.pattern("^[a-zA-Z0-9]{8,18}$"));
+  this.formSignUpGroup.addControl('UserPasswordControl',
+                           new FormControl('', Validators.compose(validationCollectionPassword)));
+  this.formSignUpGroup.addControl('UserPinControl', 
+                           new FormControl('', Validators.compose(validationCollection)));
+}
+
+ngOnInit() {
+  this.items = this.formService.getItems();
+  console.log(this.items);
+}
+addUser(User){
+  this.formService.addUser(this.UserModel);
+  this.formService.createCustomer(this.UserModel);
+}
+hasError(controlString: string, typeOfValidatior: string): boolean{
+  return this.formSignUpGroup.controls[controlString].hasError(typeOfValidatior);
+}
+}
